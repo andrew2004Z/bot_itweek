0cf5264f0df686ec61c791c0fe8b4f44171c2a47
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2022-04-22 12:13:01 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,17 +11,7 @@ from config import TOKEN, ADMIN_ID
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 db = WorkDB('db.db')
-a = '''UhxPb*EW@1%3
-3iGgwmFC3p~U
-HZBIuo7$YnrT
-IlYY0aDKOSIN
-8OOXhH*jMVX@
-UJT*|XDhokjd
-~6ga0Ec6Z|Ji
-w%Zdm09@fpDe
-|{c1|aX3*PfO
-#a9w67ir#}TL'''.split()
-print(a)
+answer = ['UhxPb*EW@1%3', '3iGgwmFC3p~U', 'HZBIuo7$YnrT', 'IlYY0aDKOSIN', '8OOXhH*jMVX@', 'UJT*|XDhokjd', '~6ga0Ec6Z|Ji', 'w%Zdm09@fpDe', '|{c1|aX3*PfO', '#a9w67ir#}TL']
 @dp.message_handler(commands=['start'])
 async def process_start_command(msg: types.Message):
     if db.check_user(msg.from_user.id) == False:
@@ -35,16 +25,27 @@ async def process_start_command(msg: types.Message):
         raiting = db.get_top_rating()
         raiting = sort_rating(raiting)
         sp = []
-        for i in raiting:
-            sp.append(f'{i[1]} : {i[2]}')
+        for j, i in enumerate(raiting):
+            if str(i[2]) != '0':
+                sp.append(f'{j}. {i[1]} : {i[2]}')
         await bot.send_message(msg.from_user.id, '\n'.join(sp))
-        print(db.ge)
+        #print(db.ge)
     else:
         await msg.reply("Извините, похоже вы не админ.")
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+    a = db.get_users_train(msg.from_user.id)[3:-3].split("', '")
+    print(a)
+    if msg.text in a:
+        db.update_score(msg.from_user.id)
+        a.remove(msg.text)
+        db.update_answer(msg.from_user.id, a)
+        await bot.send_message(msg.from_user.id, f'{msg.text} : парвильный ответ \n У вас {db.get_score(msg.from_user.id)} очков.')
+    elif msg.text in answer:
+        await bot.send_message(msg.from_user.id, f'{msg.text} : парвильный ответ, но вы уже его вводили. \nУ вас {db.get_score(msg.from_user.id)} очков.')
+    else:
+        await bot.send_message(msg.from_user.id, f'{msg.text} : не является ответом')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- sql.py
+++ sql.py
@@ -20,8 +20,17 @@ class WorkDB:
         with self.connection:
             return self.cursor.execute('''INSERT INTO 'answer' (id, answer)  VALUES  (?,?)''', (user_id, answer))
 
-    def update_score(self, user_id, score):
+    def get_score(self, id):
+        get_q = f'SELECT score FROM users WHERE id = {id};'
         with self.connection:
+            result = self.cursor.execute(get_q).fetchall()
+            res = str(result[0])[1:]
+            res = res[:-2]
+            return int(res)
+
+    def update_score(self, user_id):
+        with self.connection:
+            score = self.get_score(user_id) + 1
             return self.cursor.execute('''UPDATE `users` SET `score` = ? WHERE `id` = ?''', (score, user_id))
 
     def get_username(self, user_id):
@@ -31,6 +40,7 @@ class WorkDB:
             res = str(result[0])[1:]
             res = res[:-2]
             return str(res)
+    
 
     def get_users_train(self, user_id):
         get_q = f'SELECT answer FROM answer WHERE id = {user_id};'
@@ -41,237 +51,8 @@ class WorkDB:
             return str(res)
 
 
-
-
-
-
-
-    def get_username(self, user_id):
-        get_q = f'SELECT username FROM users WHERE user_id = {user_id};'
+    def update_answer(self, user_id, answer):
         with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return str(res)
+            return self.cursor.execute('''UPDATE `answer` SET `answer` = ? WHERE `id` = ?''',
+                                       (str(answer), user_id))
 
-
-    def update_tasks_id(self, user_id, tasks_id):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_train` SET `tasks_id` = ? WHERE `user_id` = ?''',
-                                       (str(tasks_id), user_id))
-
-    
-
-    def update_date_use(self, user_id, date_use):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `users` SET `date_last_use` = ? WHERE `user_id` = ?''',
-                                       (date_use, user_id))
-
-    def update_task_id(self, user_id, task_id):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_task` SET `task_id` = ? WHERE `user_id` = ?''',
-                                       (task_id, user_id))
-
-    def add_user_in_user_task(self, user_id, task_id):
-        with self.connection:
-            return self.cursor.execute('''INSERT INTO 'user_task' (user_id, task_id)  VALUES  (?,?)''',
-                                       (user_id, task_id))
-
-    def create_static(self, user_id, col_true, col_false, staicstic, rating):
-        with self.connection:
-            return self.cursor.execute('''INSERT INTO 'user_statistic' (user_id,сol_true_answer,col_false_answer,col_resh, rating)  VALUES  (?,?,?,?,?)''',
-                                       (user_id, col_true, col_false, staicstic, rating))
-
-    def get_rating_diapason(self, min_d, max_d):
-        with self.connection:
-            result = self.cursor.execute(f'SELECT * FROM tasks WHERE rating BETWEEN {min_d} AND {max_d}').fetchall()
-            return result
-
-    def update_col_true(self, user_id, col_true):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_statistic` SET `сol_true_answer` = ? WHERE `user_id` = ?''',
-                                       (col_true, user_id))
-
-    def update_col_false(self, user_id, col_false):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_statistic` SET `сol_false_answer` = ? WHERE `user_id` = ?''',
-                                       (col_false, user_id))
-
-    def update_col_resh(self, user_id, stat):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_statistic` SET `col_resh` = ? WHERE `user_id` = ?''',
-                                       (stat, user_id))
-
-    def close_connection(self):
-        self.sqlite_connection.close()
-        return True
-
-    def get_task_id_user(self, user_id):
-        get_q = f'SELECT task_id FROM user_task WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return str(res)
-
-    def get_statistic(self, user_id):
-        get_q = f'SELECT * FROM user_statistic WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            return result
-
-    def get_task(self, id):
-        with self.connection:
-            result = self.cursor.execute('SELECT * FROM tasks WHERE id = ?', (id,)).fetchall()
-            return result
-    
-    def get_task_answer(self, id):
-        with self.connection:
-            result = self.cursor.execute('SELECT answer FROM tasks WHERE id = ?', (id,)).fetchall()
-            return result
-        
-    def get_all_task(self, number_task):
-        get_q = f'SELECT * FROM tasks WHERE number_task = {number_task};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            return result
-
-    def update_col_quetions(self, user_id, count_quetions):
-        """Обновляем статус подписки пользователя"""
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_info` SET `count_quetions` = ? WHERE `user_id` = ?''',
-                                       (count_quetions, user_id))
-
-    def update_date_use(self, user_id, date_use):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `users` SET `date_last_use` = ? WHERE `user_id` = ?''',
-                                       (date_use, user_id))
-
-    def get_top_rating(self):
-        get_q = f'SELECT * FROM users;'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            all_users = []
-            for item in result:
-                all_users.append([item[0], item[1], item[2]])
-            return all_users
-
-
-
-
-    def update_task_num_attempts(self, id):
-        with self.connection:
-            num_attempts = self.get_task_num_attempts(id) + 1
-            return self.cursor.execute('''UPDATE `tasks` SET `num_attempts` = ? WHERE `id` = ?''',
-                                       (num_attempts, id))
-
-    def get_task_num_attempts(self, id):
-        get_q = f'SELECT num_attempts FROM tasks WHERE id = {id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-    
-    def update_task_rating(self, id):
-        with self.connection:
-            rating = (self.get_task_rights_solves(id) / self.get_task_num_attempts(id) * 100)
-            return self.cursor.execute('''UPDATE `tasks` SET `rating` = ? WHERE `id` = ?''',
-                                       (rating, id))
-
-    def update_task_rights_solves(self, id):
-        with self.connection:
-            num_attempts = self.get_task_rights_solves(id) + 1
-            return self.cursor.execute('''UPDATE `tasks` SET `rights_solves` = ? WHERE `id` = ?''',
-                                       (num_attempts, id))
-
-    def get_task_rights_solves(self, id):
-        get_q = f'SELECT rights_solves FROM tasks WHERE id = {id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-
-    def get_task_сol_true_answer(self, user_id):
-        get_q = f'SELECT сol_true_answer FROM user_statistic WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-
-    def get_user_rating(self, user_id):
-        get_q = f'SELECT rating FROM user_statistic WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-
-    def get_task_col_false_answer(self, user_id):
-        get_q = f'SELECT col_false_answer FROM user_statistic WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-
-    def update_task_сol_true_answer(self, user_id):
-        with self.connection:
-            сol_true_answer = self.get_task_сol_true_answer(user_id) + 1
-            return self.cursor.execute('''UPDATE `user_statistic` SET `сol_true_answer` = ? WHERE `user_id` = ?''',
-                                       (сol_true_answer, user_id))
-
-    def update_user_rating(self, user_id, rating_Add):
-        with self.connection:
-            rating = self.get_user_rating(user_id) + (101 - rating_Add)
-            return self.cursor.execute('''UPDATE `user_statistic` SET `rating` = ? WHERE `user_id` = ?''',
-                                       (rating, user_id))
-    
-    def update_task_col_false_answer(self, user_id):
-        with self.connection:
-            col_false_answer = self.get_task_col_false_answer(user_id) + 1
-            return self.cursor.execute('''UPDATE `user_statistic` SET `col_false_answer` = ? WHERE `user_id` = ?''',
-                                       (col_false_answer, user_id))
-
-    def update_task_col_resh(self, user_id, col_resh):
-        with self.connection:
-            return self.cursor.execute('''UPDATE `user_statistic` SET `col_resh` = ? WHERE `user_id` = ?''',
-                                       (col_resh, user_id))
-
-    def get_task_rating(self, id):
-        get_q = f'SELECT rating FROM tasks WHERE id = {id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
-
-    
-
-    def get_task_col_resh(self, user_id):
-        get_q = f'SELECT col_resh FROM user_statistic WHERE user_id = {user_id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return res
-
-    def get_task_number_id(self, id):
-        get_q = f'SELECT number_task FROM tasks WHERE id = {id};'
-        with self.connection:
-            result = self.cursor.execute(get_q).fetchall()
-            res = str(result[0])[1:]
-            res = res[:-2]
-            print(res)
-            return int(res)
