7e9376b772cb83f6f1c4025e7eb378a25f25b2bc
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2022-04-28 15:47:12 +0300

    Твои Qr-Коды

---
+++ 1/1.py
@@ -0,0 +1,9 @@
+import random
+
+answer = ['UhxPb*EW@1%3', '3iGgwmFC3p~U', 'HZBIuo7$YnrT', 'IlYY0aDKOSIN', '8OOXhH*jMVX@', 'UJT*|XDhokjd', '~6ga0Ec6Z|Ji', 'w%Zdm09@fpDe', '|{c1|aX3*PfO', '#a9w67ir#}TL']
+a = ''
+for i in answer:
+    a += i[0]
+answer.append(a+'kA')
+for i in answer:
+    print(i)
